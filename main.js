64b42fea6c201bb4ac1e7d9d432d95fe32b3607a
glorymugizi <glorymugizi@gmail.com>
2022-05-19 20:02:26 +0300

    modified index file and js file

--- main.js
+++ main.js
@@ -1,12 +1,520 @@
-const entireHeading = document.querySelector("#heading");
-const childHeading = document.querySelector("#heading article");
-const button = document.querySelector(".button");
-function headingOne(){
-entireHeading.innerHTML="CHANGED HEADING BY FUNCTION";
-// console.log("heading one")
+"use strict";
+
+// const entireHeading = document.querySelector("#heading");
+// const childHeading = document.querySelector("#heading article");
+// const button = document.querySelector(".button");
+// function headingOne() {
+//   entireHeading.innerHTML = "CHANGED HEADING BY FUNCTION";
+//   // console.log("heading one")
+// }
+// function changChildHeading() {
+//   childHeading.innerHTML = "CHANGED CHILD HEADING BY FUNCTION";
+//   // console.log("heading one")
+// }
+// button.addEventListener("click", changChildHeading);
+
+// function myParagraphTest(a, b) {
+//   return a * b;
+// }
+// console.log(myParagraphTest(2, 3));
+// document.getElementById("testPara").innerHTML = myParagraphTest(3, 9);
+
+// const cars = ["toyota", "volvo", "benz", "mazda"];
+// document.getElementById("testPara").innerHTML = cars;
+// console.log(cars);
+// console.log(`${cars[0]}, ${cars[2]}`);
+
+// names = new Array("glory", "mugizi", "ivan");
+
+// console.log(names);
+// const owner = cars.concat(names);
+// console.log(owner);
+// console.log(names.copyWithin(2));
+// console.log(names.join(cars));
+
+// const today = new Date();
+// const name = "glory";
+// console.log(name);
+// function myName() {
+//   return()
+// }
+/*let age = 40;
+age = 50;
+const birthYear = 2022 - age;
+
+console.log(`He was born in ${birthYear} and his  is  a ${age} year old man`);
+let m = 30 + 8;
+m += 10;
+m++;
+console.table(m);*/
+
+/*const markMass = 78;
+const johnMass = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+console.log(`Mark's BMI is ${markBMI} and John's ${johnBMI}`);
+
+const markHigherBM1 = markBMI > johnBMI;
+console.log(markHigherBM1);
+if (markBMI > johnBMI) {
+  console.log(`mark is heavier than john`);
+} else console.log(`John is lighter than mark`);
+
+const voter = 15;
+const isOfVotingAge = voter >= 18;
+if (isOfVotingAge) {
+  console.log(`${voter} is of a voting age`);
+} else {
+  console.log(`he is too young to vote 🇺🇬`);
 }
-function changChildHeading(){
-  childHeading.innerHTML="CHANGED CHILD HEADING BY FUNCTION";
-  // console.log("heading one")
+
+const birthYear = 2003;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+
+const inputYear = "1999";
+console.log(Number(inputYear) + 18, inputYear);
+console.log(inputYear + 18);
+const ages = 23;
+console.log(String(ages), ages);
+
+console.log(inputYear + ages);
+
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return factorial(n - 1) * n;
   }
-button.addEventListener("click",changChildHeading);
+}
+console.log(factorial(8));
+console.log(factorial(3));
+console.log(`Greetings everyone \nhope you all had a wonderful day`);
+console.log(`"this is a secondline\t\t\t\\"`);
+console.log(typeof 4.5);
+console.log(typeof "x");
+console.log(-(10 - 2));
+
+const select = 5;
+if (select == 1) {
+  console.log(`you have switched on`);
+} else {
+  console.log(`switched off`);
+}
+console.log("glory" || "1");
+let caught = 5 * 5;
+console.log(caught);
+let ten = 10;
+console.log(ten * ten);
+let mood = "light";
+console.log(mood);
+mood = "dark";
+console.log(mood);
+let nelson = 140;
+nelson = nelson - 35;
+console.log(nelson);
+let jimmy;
+let one = 1,
+  two = 2;
+console.log(one + two);
+console.log(Math.max(6, 8));
+console.log(Math.max(6, 8) + 200);*/
+/*let theNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(theNumber)) {
+  console.log("your number is the square root of " + theNumber * theNumber);
+} else {
+  console.log(`hey.why didn't you give me a number?`);
+}
+
+let num = Number(prompt("pick a number"));
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("large");
+}*/
+
+/*let numbers = 0;
+while (numbers <= 12) {
+  console.log(numbers);
+  numbers = numbers + 2;
+}*/
+
+/*numbers = Number(prompt("pick a number"));
+if (numbers / 2 === 0) {
+  console.log("you have picked an even number");
+} else {
+  console.log("the number picked is an odd number");
+}*/
+/*let yourName;
+do {
+  yourName = prompt("Who are you?");
+} while (!yourName);
+console.log(yourName);
+
+if (false != true) {
+  console.log("that makes sense.");
+  if (1 < 2) {
+    console.log("no surprise there");
+  }
+}*/
+// for (let numbers = 0; numbers <= 12; numbers = numbers + 2) {
+//   console.log(numbers);
+// }
+
+// for (let counter = 0; counter < 10; counter = counter + 1) {
+//   console.log(counter);
+// }
+// let result = 1;
+// for (let counter = 0; counter < 10; counter = counter + 1) {
+//   result = result * 2;
+// }
+// console.log(result);
+// for (let current = 20; ; current = current + 1) {
+//   if (current % 7 == 0) {
+//     console.log(current);
+//     break;
+//   }
+// }
+// switch (prompt(`what is the weather like?`)) {
+//   case "rainy":
+//     console.log(`come with an umbrella`);
+//     break;
+//   case "sunny":
+//     console.log("dress lightly");
+//     break;
+//   case "cloudy":
+//     console.log("Go outside");
+//     break;
+//   default:
+//     console.log("unknown weather type!");
+// }
+
+//triangle.
+
+// for (let triangle = "#"; triangle.length < 8; triangle = triangle += "#") {
+//   console.log(triangle);
+// }
+
+// for (let num = 1; num <= 100; num = num + 1) {
+//   let output = "";
+//   if (num % 3 == 0) {
+//     console.log("Fizz");
+//   }
+//   if (num % 5 == 0 && num % 3 == 0) {
+//     console.log("FizzBuzz");
+//   }
+//   console.log(output || num);
+// }
+/*for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
+for (let trophy = "🏆"; trophy.length < 15; trophy = trophy += "🏆") {
+  console.log(trophy);
+}
+//chessboard
+let size = 8;
+let board = "";
+for (y = 0; y < size; y++) {
+  for (x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
+
+i = 1;
+console.log(i++);
+console.log(i++);
+console.log(i++);
+console.log(i++);
+console.log(i++);
+
+const square = function (x) {
+  return x * x;
+};
+console.log(square(12));
+
+// const theReminder = function (a) {
+//   return a ** 2;
+// };
+// console.log(theReminder(13));
+
+// const makeNoise = function () {
+//   console.log("ping");
+// };
+// makeNoise();
+
+// const power = function (base, exponent) {
+//   let result = 1;
+//   for (let count = 0; count < exponent; count++) {
+//     result *= base;
+//   }
+//   return result;
+// };
+// console.log(power(10, 0));
+
+// let a = 10;
+// if (true) {
+//   var b = 20;
+//   let c = 30;
+//   console.log(a + b + c);
+// }
+// console.log(a + c);
+
+/* const halve = function (n) {
+  console.log(n / 2);
+};
+let n = 10;
+halve(30);
+const humus = function (factor) {
+  const ingredient = function (amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "chickpeas");
+  ingredient(0.5, "teaspoon", "cumin");
+};
+console.log(humus(10));
+
+for (triangle = "#"; triangle.length < 8; triangle = triangle + "#") {
+  console.log(triangle);
+}
+
+const bmi = function (mass, height) {
+  return mass / height ** 2;
+};
+console.log(bmi(10, 2));
+
+let result = 1;
+for (counter = 0; counter < 20; counter = counter + 1) {
+
+  }
+  result = result * 2;
+}
+console.log(result);
+*/
+/*const halve = function (n) {
+  return n / 2;
+};
+let n = 10;
+console.log(halve(100));
+console.log(n);
+
+const mandazi = function (multiplier) {
+  const ingredients = function (amount, unit, name) {
+    let ingredientsAmount = amount * multiplier;
+    if (ingredientsAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientsAmount} ${unit} ${name}`);
+  };
+  ingredients(2, "cup", "flour");
+  ingredients(1, "cup", "water");
+  ingredients(2.5, "teaspoon", "baking powder");
+  ingredients(2, "egg", "");
+};
+console.log(mandazi(3));
+
+console.log("The future says: ", future());
+function future() {
+  return "You'll never have flying cars";
+}
+
+const power = (base, exponent) => {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+};
+console.log(power(12, 2));
+*/
+/*const horn = () => {
+  console.log("Toot");
+};
+const square1 = (x) => {
+  return x * x;
+};
+console.log(square1(3));
+
+const square2 = (x) => x * x;
+console.log(square2(4));
+console.log(horn());
+function greet(who) {
+  console.log("hello " + who);
+}
+greet("harry");
+console.log("bye");
+function chicken() {
+  return egg();
+}
+function egg() {
+  return chicken();
+}
+console.log(chicken() + "came first");*/
+/*function square(x) {
+  return x * x;
+}
+console.log(square("hedgehog"));
+
+function minus(a, b) {
+  if (b === undefined) return -a;
+  else return a - b;
+}
+console.log(minus(10, 30));
+
+function wrapValue(n) {
+  let local = n;
+  return () => local;
+}
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+console.log(wrap2());
+
+function multiplier(factor) {
+  return (number) => number * factor;
+}
+let twice = multiplier(3);
+console.log(twice(5));
+
+function power(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
+console.log(power(5, 2));
+
+const tellFortune = function (
+  jobTitle,
+  geographicLocation,
+  partnerName,
+  numOfchild
+) {
+  let future =
+    "you will be a " +
+    jobTitle +
+    "in " +
+    geographicLocation +
+    " and married to " +
+    partnerName +
+    " " +
+    "with " +
+    numOfchild +
+    "kids";
+
+  console.log(future);
+};
+tellFortune("teacher", "england", "Scovia", 2);
+tellFortune("Doctor", "America", "Scovia", 2);*/
+
+// const calcBmi = function (mass, height) {
+//   let bmi = mass / height ** 2;
+//   if (bmi < 18) {
+//     console.log(`your BMI is ${bmi} and you are under weight.`);
+//   } else if (bmi > 18 && bmi <= 25) {
+//     console.log(`your BMI is ${bmi} and you have a normal  weight.`);
+//   } else {
+//     console.log(`your BMI is ${bmi} and you are obese.`);
+//   }
+// };
+// console.log(calcBmi(54, 1.6));
+
+// function calculateDogAge(age) {
+//   let dogAge = 7 * age;
+//   console.log(dogAge);
+// }
+// console.log(calculateDogAge(1));
+// console.log(calculateDogAge(2));
+// console.log(calculateDogAge(3));
+
+// const averageDolphin = (97 + 112 + 101) / 3;
+// const averageKoalas = (109 + 95 + 123) / 3;
+
+// console.log(averageDolphin);
+// console.log(averageKoalas);
+
+// if (averageDolphin > averageKoalas && averageDolphin >= 100) {
+//   console.log(`Team Dolphins are winners of this Competition 🏆`);
+// } else if (averageKoalas > averageDolphin && averageKoalas >= 100) {
+//   console.log(`Team Koalas are winners of this Competition 🏆`);
+// } else if (
+//   averageDolphin === averageKoalas &&
+//   averageDolphin >= 100 &&
+//   averageKoalas >= 100
+// ) {
+//   console.log(`Its a draw and rematch should be scheduled in two weeks.`);
+// } else {
+//   console.log(`There is no winner`);
+// }
+
+// let money = 100;
+// if (money) {
+//   console.log(`dont spend it all`);
+// } else {
+//   console.log(`you dont have money`);
+// }
+// const age = "18";
+// if (age === 18) console.log(`he is an adult`);
+// else console.log(`you are still young`);
+// console.log(!age == 18);
+
+// const driversLicense = true;
+// const goodVision = true;
+// const tired = false;
+// const canDrive = driversLicense && goodVision && !tired;
+// if (canDrive) {
+//   console.log(`Sarah can drive`);
+// } else {
+//   console.log(`someone else should drive`);
+// }
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `" The  bill was ${bill}, tip was ${tip} and total value ${bill + tip}" `
+);
+
+// const day = prompt("what is the day of the week");
+// switch (day) {
+//   case "monday":
+//     console.log(`plan the course meeting`);
+//     break;
+//   case "Tuesday":
+//     console.log(`Attend the coding class`);
+//     break;
+// }
+
+// const age = 23;
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+const bmi = (mass, height) => {
+  return mass / height ** 2;
+};
+console.log(bmi(78, 1.9));
